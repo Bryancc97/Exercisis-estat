65a5c1aa79dba88aec7849ec42e52aa1a94e2194
Bryan C <bryan.carrera.7e8@itb.cat>
2025-03-12 09:45:36 +0100

    Ejercicios por terminar

---
+++ app/src/main/java/com/example/exercicisstateless/MainActivity.kt
@@ -0,0 +1,163 @@
+package com.example.exercicisstateless
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.graphics.createBitmap
+import com.example.exercicisstateless.ui.theme.ExercicisStatelessTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ExercicisStatelessTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Exercici2(modifier = Modifier.padding(innerPadding))
+                    //Exercici3(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+@Composable
+fun Exercici2(modifier: Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.ic_task_completed),
+        contentDescription = "TAREA TERMINADA",
+        alpha = 1f
+
+    )
+
+}
+
+//EJERCICIO 3 TERMINADO
+@Composable
+fun Exercici3(modifier: Modifier) {
+    Column(modifier.fillMaxSize()) {
+        Row(
+            modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            Column(
+                modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color(0xFFEADDFF))
+                    .padding(horizontal = 12.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    "Text composable",
+                    textAlign = TextAlign.Center,
+                    modifier = modifier.fillMaxWidth(),
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    "Displays text and follows the recommended Material Design guidelines.",
+                    textAlign = TextAlign.Justify,
+                    modifier = modifier.fillMaxWidth()
+                )
+            }
+            Column(
+                modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color(0xFFD0BCFF))
+                    .padding(horizontal = 12.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    "Image composable",
+                    textAlign = TextAlign.Center,
+                    modifier = modifier.fillMaxWidth(),
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    "Creates a composable that lays out and draws a given Painter class object.",
+                    textAlign = TextAlign.Justify,
+                    modifier = modifier.fillMaxWidth()
+                )
+            }
+        }
+
+        Row(
+            modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            Column(
+                modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color(0xFFD0BCFF))
+                    .padding(horizontal = 12.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    "Row composable",
+                    textAlign = TextAlign.Center,
+                    modifier = modifier.fillMaxWidth(),
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    "A layout composable that places its children in a horizontal sequence.",
+                    textAlign = TextAlign.Justify,
+                    modifier = modifier.fillMaxWidth()
+                )
+            }
+            Column(
+                modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color(0xFFEADDFF))
+                    .padding(horizontal = 12.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    "Column composable",
+                    textAlign = TextAlign.Center,
+                    modifier = modifier.fillMaxWidth(),
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    "A layout composable that places its children in a vertical sequence.",
+                    textAlign = TextAlign.Justify,
+                    modifier = modifier.fillMaxWidth()
+                )
+            }
+        }
+    }
+
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    ExercicisStatelessTheme {
+        Exercici2(modifier = Modifier)
+        //Exercici3(modifier = Modifier)
+    }
+}
